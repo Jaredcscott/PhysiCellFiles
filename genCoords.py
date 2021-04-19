e67c01e83fe33dfe32e674e0dabe242f099bc71f
jaredcscott <jaredcscott93@gmail.com>
2021-04-19 00:01:11 -0600

    adding genCoords

--- genCoords.py
+++ genCoords.py
@@ -4,19 +4,16 @@ import matplotlib.patches as mpatches
 
 ox = 0
 oy = 0
-ndiscs=50
+points=50
 offset=0.0
-tau=(1+5**0.5)/2.0 # golden ratio approx = 1.618033989
-#(2-tau)*2*np.pi is golden angle = c. 2.39996323 radians, or c. 137.5 degrees
+tau=(1+5**0.5)/2.0          # golden ratio approx = 1.618033989
 inc = (2-tau)*2*np.pi + offset
 theta=0
 k=0.1 # scale factor
 drad=k*(1+5**0.5)/4.0 # radius of each disc
 minv=maxv=0 # minv and maxv will be used later to display inputs chosen
 
-# now collect in list 'patches' the locations of all the discs
-patches = []
-for j in range(1,ndiscs+1):
+for j in range(1,points+1):
     r = k*j**0.5
     theta += inc
     x = ox + r*np.cos(theta)
